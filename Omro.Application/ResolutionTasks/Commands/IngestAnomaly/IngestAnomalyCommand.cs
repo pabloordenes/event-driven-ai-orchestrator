6753b107e010f2614c467cab5fdfbba468bd73c9
Pablo <pabloinfortech@gmail.com>
2026-08-26 13:39:33 -0400

    feat: implementacion de IngestAnomalyCommand y su handler correspondiente

---
+++ Omro.Application/ResolutionTasks/Commands/IngestAnomaly/IngestAnomalyCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Omro.Application.ResolutionTasks.Commands.IngestAnomaly;
+
+public record IngestAnomalyCommand(
+    decimal RiskProbability,
+    Guid ResourceId
+    ) : IRequest<Guid>;
---
+++ Omro.Application/ResolutionTasks/Commands/IngestAnomaly/IngestAnomalyCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Omro.Domain.ResolutionTasks;
+
+namespace Omro.Application.ResolutionTasks.Commands.IngestAnomaly;
+
+public class IngestAnomalyCommandHandler : IRequestHandler<IngestAnomalyCommand, Guid>
+{
+    public Task<Guid> Handle(IngestAnomalyCommand request, CancellationToken cancellationToken)
+    {
+        var anomalyEvent = new AnomalyEvent(
+            RiskProbability: request.RiskProbability,
+            ResourceId: request.ResourceId);
+        
+        ResolutionTask resolutionTask = new(anomalyEvent);
+
+        return Task.FromResult(resolutionTask.Id);
+    }
+}
