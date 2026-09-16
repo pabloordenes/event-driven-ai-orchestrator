890d6e7e918b81ef6ba21d186434cd688215d0af
Pablo <pabloinfortech@gmail.com>
2026-09-15 22:12:58 -0300

    test(domain): implementar pruebas unitarias para el aggregate root resolutiontask

---
+++ Omro.Domain.Tests/ResolutionTasks/ResolutionTaskTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Omro.Domain.ResolutionTasks;
+
+namespace Omro.Domain.Tests.ResolutionTasks;
+
+public class ResolutionTaskTests
+{
+    [Fact]
+    public void StartAnalysis_WhenStateIsIngested_ShouldChangeToAnalyzing()
+    {
+        // arrange
+        var dummyEvent = new AnomalyEvent(RiskProbability:0.8m, ResourceId:Guid.NewGuid());
+        
+        var task = new ResolutionTask(dummyEvent);
+        
+        // act
+        task.StartAnalysis();
+        
+        // assert
+        // verificamos que el estado cambio usando FluentAssertions
+        task.Status.Should().Be(ResolutionStatus.Analyzing);
+        
+        // verificamos que se haya agregado exactamente 1 registro a la lista
+        task.ReasoningTraces.Should().HaveCount(1);
+        
+        // verificamos keywords
+        task.ReasoningTraces[0].Should().Contain("Iniciando motor cognitivo");
+    }
+
+    [Fact]
+    public void StartAnalysis_WhenStateIsNotIngested_ShouldThrowException()
+    {
+        
+        // arrange
+        var dummyEvent = new AnomalyEvent(RiskProbability:0.8m, ResourceId:Guid.NewGuid());
+        
+        var task = new ResolutionTask(dummyEvent);
+        
+        // act
+        task.StartAnalysis(); // pasamos el estado a analyzing para forzar la excepcion
+        
+        Action act = () => task.StartAnalysis();
+        
+        // assert
+        act.Should().Throw<InvalidOperationException>();
+    }
+}
